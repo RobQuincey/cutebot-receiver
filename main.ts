c4fc889a0d142ccb5bffc5b2968562029b84e0ed
RobQuincey <rpquincey@hotmail.com>
2022-12-07 17:46:23 +0000

    Break code into functions

--- main.ts
+++ main.ts
@@ -1,80 +1,96 @@
+function turnLeft () {
+    cuteBot.motors(50, 100)
+    cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0x00ff00)
+    cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x000000)
+    leftled.showColor(neopixel.colors(NeoPixelColors.Green))
+    rightled.showColor(neopixel.colors(NeoPixelColors.Black))
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+}
+function goForward () {
+    cuteBot.motors(100, 100)
+    cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x00ff00)
+    strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+}
+function stop () {
+    cuteBot.stopcar()
+    cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff0000)
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+}
+function reverse () {
+    stop()
+    cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0xff8000)
+    cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff8000)
+    rightled.showColor(neopixel.colors(NeoPixelColors.Orange))
+    leftled.showColor(neopixel.colors(NeoPixelColors.Orange))
+    basic.pause(500)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+    cuteBot.moveTime(cuteBot.Direction.backward, 50, 2)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff0000)
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+}
+function turnRight () {
+    cuteBot.motors(100, 50)
+    cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0x000000)
+    cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x00ff00)
+    leftled.showColor(neopixel.colors(NeoPixelColors.Black))
+    rightled.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+}
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "forward") {
-        cuteBot.motors(100, 100)
-        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x00ff00)
-        strip.showColor(neopixel.colors(NeoPixelColors.Green))
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
+        goForward()
     } else if (receivedString == "stop") {
-        cuteBot.stopcar()
-        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff0000)
-        strip.showColor(neopixel.colors(NeoPixelColors.Red))
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # # # .
-            . # # # .
-            . . . . .
-            `)
+        stop()
     } else if (receivedString == "left") {
-        cuteBot.motors(50, 100)
-        cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0x00ff00)
-        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x000000)
-        leftled.showColor(neopixel.colors(NeoPixelColors.Green))
-        rightled.showColor(neopixel.colors(NeoPixelColors.Black))
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
+        turnLeft()
     } else if (receivedString == "right") {
-        cuteBot.motors(100, 50)
-        cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0x000000)
-        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x00ff00)
-        leftled.showColor(neopixel.colors(NeoPixelColors.Black))
-        rightled.showColor(neopixel.colors(NeoPixelColors.Green))
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
+        turnRight()
     } else if (receivedString == "reverse") {
-        cuteBot.stopcar()
-        cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0xff8000)
-        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff8000)
-        rightled.showColor(neopixel.colors(NeoPixelColors.Orange))
-        leftled.showColor(neopixel.colors(NeoPixelColors.Orange))
-        basic.pause(500)
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # . # . #
-            . # # # .
-            . . # . .
-            `)
-        cuteBot.moveTime(cuteBot.Direction.backward, 50, 2)
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # # # .
-            . # # # .
-            . . . . .
-            `)
-        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff0000)
-        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        reverse()
     } else {
     	
     }
 })
+// Set up LEDs and assign radio group
 let rightled: neopixel.Strip = null
 let leftled: neopixel.Strip = null
 let strip: neopixel.Strip = null
